038e92b59ca7e9bea91be6073af355ddad2af777
Osaro Iyoha <saro.iyoha@gmail.com>
2023-12-27 19:02:07 +0100

    ReactNative Dimensions API

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import Colors from '../../constants/colors'
 
 function NumberContainer({children}) {
@@ -10,13 +10,15 @@ function NumberContainer({children}) {
   )
 }
 
-export default NumberContainer
+export default NumberContainer;
+
+const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
     container : {
         borderWidth: 4,
         borderColor: Colors.accent500,
-        padding: 24,
+        padding: deviceWidth < 380 ? 12 : 24,
         margin: 24,
         borderRadius: 8,
         alignItems: 'center',
@@ -25,7 +27,7 @@ const styles = StyleSheet.create({
     },
     numberText: {
         color: Colors.accent500,
-        fontSize: 36,
+        fontSize: deviceWidth < 380 ? 28 : 36,
         // fontWeight: 'bold'
         fontFamily: 'open-sans-bold',
     }
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, Image, StyleSheet} from 'react-native'
+import { Text, View, Image, StyleSheet, Dimensions} from 'react-native'
 import Title from '../components/ui/Title'
 import Colors from '../constants/colors'
 import PrimaryButton from '../components/ui/PrimaryButton'
@@ -9,7 +9,7 @@ function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
     <View style={styles.rootContainer}>
       <Title>Game Over</Title>
       <View style={styles.imageContainer}>
-      <Image style={styles.image} source={require('../assets/images/success.png')} />
+        <Image style={styles.image} source={require('../assets/images/success.png')} />
       </View>
       <Text style={styles.summaryText}>Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess <Text style={styles.highlight}>{userNumber}</Text>.</Text>
       <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
@@ -17,7 +17,9 @@ function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
   )
 }
 
-export default GameOverScreen
+export default GameOverScreen;
+
+const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   rootContainer: {
@@ -27,9 +29,9 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: deviceWidth < 380 ? 150 : 300,
+    height: deviceWidth < 380 ? 150 : 300,
+    borderRadius: deviceWidth < 380 ? 75 : 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: 'hidden',
